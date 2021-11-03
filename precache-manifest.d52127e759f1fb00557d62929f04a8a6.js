36371eb899469e8c9a7c8669b74f8103433d7126
hektorone <doccarloscuelo@gmail.com>
2021-11-03 04:29:03 +0100

    Updates

---
+++ precache-manifest.d52127e759f1fb00557d62929f04a8a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f3a4faadb545afca63f4b847dfa54a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "fdc676a8824794b88067",
+    "url": "/resume/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e4d287fc5bd534708a81",
+    "url": "/resume/static/css/main.1f19c46e.chunk.css"
+  },
+  {
+    "revision": "fdc676a8824794b88067",
+    "url": "/resume/static/js/2.4f0fe0b0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/resume/static/js/2.4f0fe0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d287fc5bd534708a81",
+    "url": "/resume/static/js/main.4a63ad28.chunk.js"
+  },
+  {
+    "revision": "5ffb7969dfad69c660bd",
+    "url": "/resume/static/js/runtime-main.4ddf6a8f.js"
+  },
+  {
+    "revision": "a9b3932e514b9ed7ef57a19002345243",
+    "url": "/resume/static/media/osagie.a9b3932e.jpg"
+  }
+]);
